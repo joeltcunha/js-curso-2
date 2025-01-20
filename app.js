9b024b1a7b4d0df280eb579de599294197870680
Joel T Cunha <joelcunha6@gmail.com>
2025-01-20 15:54:17 -0300

    Commit
    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+let titulo = document.querySelector('h1');
+titulo.textContent = 'Hora do Desafio';
+
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+function verificarChute() {
+    console.log('O botão foi clicado.');
+
+}
+// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+function alerta() {
+    alert('Eu amo JS');
+}
+
+// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+function promptCidade() {
+    let cidade = promptCidade(prompt('Digite o nome de uma cidade do Brasil:'))
+    console.log(promptCidade())
+}
+
+// Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
